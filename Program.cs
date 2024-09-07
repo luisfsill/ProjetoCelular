bfcf3bf04cb859c9c03b8b89879c11f2803cc808
luisfsill <luisfsill@hotmail.com>
2024-09-07 10:41:19 -0300

    Criaçao do codigo main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Console.WriteLine("Testando o Nokia:");
+        Smartphone nokia = new Nokia("987654321", "Nokia 3310", "222222222222222", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Console.WriteLine("Testando o iPhone:");
+        Smartphone iphone = new Iphone("123456789", "iPhone 13", "111111111111111", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
