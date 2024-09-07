f3a8deb06691a2048e8a9e5832919698becef232
luisfsill <luisfsill@hotmail.com>
2024-09-07 10:40:38 -0300

    Completando codigos das classes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,32 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; } // Propriedade para o modelo do smartphone
+        public string IMEI { get; set; }   // Propriedade para o número IMEI do smartphone
+        public int Memoria { get; set; }   // Propriedade para a capacidade de memória do smartphone
 
-        public Smartphone(string numero)
+        // Construtor que recebe todos os parâmetros e os atribui às propriedades
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
+        // Método para ligar o smartphone
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
+        // Método para receber uma ligação
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato para instalar aplicativos, deve ser implementado pelas classes derivadas
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
